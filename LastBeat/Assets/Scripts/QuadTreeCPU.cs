c88844bdc8e80576cf0a98ea52356180a75621de
DinnerPlz <joeld@qed-electronics.com>
2021-02-16 23:19:11 -0800

    qtreeref

--- LastBeat/Assets/Scripts/QuadTreeCPU.cs
+++ LastBeat/Assets/Scripts/QuadTreeCPU.cs
@@ -10,6 +10,50 @@ namespace QuadTree
 {
     public class QuadTree
     {
+        const int _MAXDEPTH = 100;
+        static public int currentDepth;
+        public static byte[,,] quadLookUp = new byte[,,] { 
+            {{0x1, 0xff },{0x1, 0x1 },{0x2, 0xff },{0x2, 0x3 },{0x3, 0x3 },{0x3, 0xff },{0x3, 0x1 },{0x3, 0x7 }},
+            {{0x0, 0x0 },{0x0, 0xff },{0x3, 0xff },{0x3, 0x3 },{0x2, 0x4 },{0x2, 0xff },{0x2, 0xff},{0x2, 0x3 }},
+            {{0x3, 0xff },{0x3, 0x1 },{0x0, 0x2 },{0x0, 0xff },{0x1, 0xff },{0x1, 0x2 },{0x1, 0x6 },{0x1, 0x0 }},
+            {{0x2, 0x0 },{0x2, 0xff },{0x1, 0x2 },{0x1, 0xff },{0x0, 0x0 },{0x0, 0x5 },{0x0, 0x2 },{0x0, 0xff }}
+        }; // pos 0x3
+        /*
+         * R, L, D, U, RU, RD, LD, LU
+         * 0xff for halt
+         * kinda supposed to be recursive(idk)
+         * https://web.archive.org/web/20120907211934/http://ww1.ucmss.com/books/LFS/CSREA2006/MSV4517.pdf
+         * i dont belive in god but i pray this makes any sense to me tommorw
+         * ok so... *hits blunt*
+         * the dimetions of the array indicate as follows :
+         * [0x0 - 0x3 the quadrent the neighbors are looking for is]
+         * [0x0 - 0x7 the eight squares that can be reffereanced]
+         * [instruction to create the address]
+         * when you are trying to find the neghbors you do as so:
+         * this is a quad at the top left square, sampling to left
+         */
+
+        public class Node
+        {
+            public Node p; // perent
+            public Node[] c; // children
+            public int r; // depth
+            byte pos; // gives relitive position 
+
+            public Node FindNeighbor(byte direction)
+            {
+                Node n = null;
+
+                
+
+                return n;
+            }
+
+
+        }
+    } // haha refactor 
+    public class QuadTreeh
+    {
         /*
          * This is the second attempt to make a quad tree
          * QuadTreeA was discarded due inabliliy to expand past a certin point
@@ -59,8 +103,9 @@ namespace QuadTree
             {
                 switch (direct)
                 {
-                    case
+                    //case
                 }
+                return null;
             }
             public bool[][] FindNeighbors()
             {
@@ -141,7 +186,7 @@ namespace QuadTree
 
 
         }
-        public QuadTree()
+        public QuadTreeh()
         {
             baseNode = new Node();
             baseNode.r = 0;
